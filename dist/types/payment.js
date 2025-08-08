993255247485e000c4675672eaa25bcefea0535d
MischaAtActics <mischa@actics.ch>
2025-08-08 20:05:29 +0200

    chore: exclude dist/ from git repository
    - Add dist/ to .gitignore with explanatory comment
    - Remove all dist/ files from git tracking
    - Build artifacts will now only be generated during CI/CD publishing
    - Consuming projects will get build artifacts via npm packages
    
    This completes the migration from git dependencies to proper npm package distribution.

--- dist/types/payment.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Payment Types for GoldSphere Platform
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=payment.js.map
--- dist/validation/currency-schemas.d.ts
+++
@@ -1,3 +0,0 @@
-import { z } from 'zod';
-export declare const CurrencySchema: z.ZodEnum<["EUR", "USD", "GBP", "CHF"]>;
-//# sourceMappingURL=currency-schemas.d.ts.map
